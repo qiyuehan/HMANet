b52c0febc7e142880e290465cb572f0009b0d3b5
Rain_snow <1184881783@qq.com>
2024-02-14 09:06:44 +1100

    Update tools.py

--- tools.py
+++ tools.py
@@ -7,13 +7,6 @@ plt.switch_backend('agg')
 
 def adjust_learning_rate(optimizer, epoch, args):
     lr_adjust = {epoch: args.learning_rate * (0.5 ** ((epoch - 1) // 1))}
-    # if args.lradj == 'type1':
-    #     lr_adjust = {epoch: args.learning_rate * (0.5 ** ((epoch - 1) // 1))}
-    # elif args.lradj == 'type2':
-    #     lr_adjust = {
-    #         2: 5e-5, 4: 1e-5, 6: 5e-6, 8: 1e-6,
-    #         10: 5e-7, 15: 1e-7, 20: 5e-8
-    #     }
     if epoch in lr_adjust.keys():
         lr = lr_adjust[epoch]
         for param_group in optimizer.param_groups:
