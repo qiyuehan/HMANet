bf22b02d15253c88d4cf6e234eb340c80a58dd64
Rain_snow <1184881783@qq.com>
2025-04-23 22:46:24 +0800

    Add files via upload

---
+++ script/main.sh
@@ -0,0 +1,262 @@
+
+# ETTh1
+ python -u LSTF_forecasting.py \
+  --data ETTh1.csv \
+  --model_name ETTh1 \
+  --pre_train 512 \
+  --seq_len 96 \
+  --pred_len 96 \
+  --fea_dim 7 \
+  --patch_size 16 \
+  --n_hidden_dim 1000 \
+  --train_percent 0.5 \
+  --epochs 10 \
+  --batch_size 32 \
+  --patience 5 \
+  --add_nodes 10 \
+  --test_percent 0.4
+
+  python -u LSTF_forecasting.py \
+  --data ETTh1.csv \
+  --model_name ETTh1 \
+  --pre_train 512 \
+  --seq_len 96 \
+  --pred_len 192 \
+  --fea_dim 7 \
+  --patch_size 16 \
+  --n_hidden_dim 1000 \
+  --train_percent 0.5 \
+  --epochs 10 \
+  --batch_size 32 \
+  --patience 5 \
+  --add_nodes 10 \
+  --test_percent 0.4
+
+  python -u LSTF_forecasting.py \
+  --data ETTh1.csv \
+  --model_name ETTh1 \
+  --pre_train 512 \
+  --seq_len 96 \
+  --pred_len 336 \
+  --fea_dim 7 \
+  --patch_size 16 \
+  --n_hidden_dim 400 \
+  --train_percent 0.5 \
+  --epochs 10 \
+  --batch_size 32 \
+  --patience 5 \
+  --add_nodes 10 \
+  --test_percent 0.4
+
+  python -u LSTF_forecasting.py \
+  --data ETTh1.csv \
+  --model_name ETTh1 \
+  --pre_train 512 \
+  --seq_len 96 \
+  --pred_len 720 \
+  --fea_dim 7 \
+  --patch_size 16 \
+  --n_hidden_dim 1000 \
+  --train_percent 0.5 \
+  --epochs 10 \
+  --batch_size 32 \
+  --patience 5 \
+  --add_nodes 10 \
+  --test_percent 0.4
+
+#ETTh2
+  python -u LSTF_forecasting.py \
+  --data ETTh2.csv \
+  --model_name ETTh2 \
+  --pre_train 512 \
+  --seq_len 96 \
+  --pred_len 96 \
+  --fea_dim 7 \
+  --patch_size 16 \
+  --n_hidden_dim 300 \
+  --train_percent 0.5 \
+  --epochs 10 \
+  --batch_size 32 \
+  --patience 5 \
+  --add_nodes 10 \
+  --test_percent 0.4
+
+  python -u LSTF_forecasting.py \
+  --data ETTh2.csv \
+  --model_name ETTh2 \
+  --pre_train 512 \
+  --seq_len 96 \
+  --pred_len 192 \
+  --fea_dim 7 \
+  --patch_size 16 \
+  --n_hidden_dim 300 \
+  --train_percent 0.5 \
+  --epochs 10 \
+  --batch_size 32 \
+  --patience 5 \
+  --add_nodes 10 \
+  --test_percent 0.4
+
+  python -u LSTF_forecasting.py \
+  --data ETTh2.csv \
+  --model_name ETTh2 \
+  --pre_train 512 \
+  --seq_len 96 \
+  --pred_len 336 \
+  --fea_dim 7 \
+  --patch_size 16 \
+  --n_hidden_dim 200 \
+  --train_percent 0.5 \
+  --epochs 10 \
+  --batch_size 32 \
+  --patience 5 \
+  --add_nodes 10 \
+  --test_percent 0.4
+
+  python -u LSTF_forecasting.py \
+  --data ETTh2.csv \
+  --model_name ETTh2 \
+  --pre_train 512 \
+  --seq_len 96 \
+  --pred_len 720 \
+  --fea_dim 7 \
+  --patch_size 16 \
+  --n_hidden_dim 300 \
+  --train_percent 0.5 \
+  --epochs 10 \
+  --batch_size 32 \
+  --patience 5 \
+  --add_nodes 10 \
+  --test_percent 0.4
+
+#ETTm1
+  python -u LSTF_forecasting.py \
+  --data ETTm1.csv \
+  --model_name ETTm1 \
+  --pre_train 512 \
+  --seq_len 96 \
+  --pred_len 96 \
+  --fea_dim 7 \
+  --patch_size 16 \
+  --n_hidden_dim 500 \
+  --train_percent 0.5 \
+  --epochs 10 \
+  --batch_size 32 \
+  --patience 5 \
+  --add_nodes 10 \
+  --test_percent 0.4
+
+  python -u LSTF_forecasting.py \
+  --data ETTm1.csv \
+  --model_name ETTm1 \
+  --pre_train 512 \
+  --seq_len 96 \
+  --pred_len 192 \
+  --fea_dim 7 \
+  --patch_size 16 \
+  --n_hidden_dim 500 \
+  --train_percent 0.5 \
+  --epochs 10 \
+  --batch_size 32 \
+  --patience 5 \
+  --add_nodes 10 \
+  --test_percent 0.4
+
+  python -u LSTF_forecasting.py \
+  --data ETTm1.csv \
+  --model_name ETTm1 \
+  --pre_train 512 \
+  --seq_len 96 \
+  --pred_len 336 \
+  --fea_dim 7 \
+  --patch_size 16 \
+  --n_hidden_dim 600 \
+  --train_percent 0.5 \
+  --epochs 10 \
+  --batch_size 32 \
+  --patience 5 \
+  --add_nodes 10 \
+  --test_percent 0.4
+
+  python -u LSTF_forecasting.py \
+  --data ETTm1.csv \
+  --model_name ETTm1 \
+  --pre_train 512 \
+  --seq_len 96 \
+  --pred_len 720 \
+  --fea_dim 7 \
+  --patch_size 16 \
+  --n_hidden_dim 800 \
+  --train_percent 0.5 \
+  --epochs 10 \
+  --batch_size 32 \
+  --patience 5 \
+  --add_nodes 10 \
+  --test_percent 0.4
+
+
+#ETTm2
+  python -u LSTF_forecasting.py \
+  --data ETTm2.csv \
+  --model_name ETTm2 \
+  --pre_train 512 \
+  --seq_len 96 \
+  --pred_len 96 \
+  --fea_dim 7 \
+  --patch_size 16 \
+  --n_hidden_dim 1000 \
+  --train_percent 0.5 \
+  --epochs 20 \
+  --batch_size 32 \
+  --patience 5 \
+  --add_nodes 10 \
+  --test_percent 0.4
+
+  python -u LSTF_forecasting.py \
+  --data ETTm2.csv \
+  --model_name ETTm2 \
+  --pre_train 512 \
+  --seq_len 96 \
+  --pred_len 192 \
+  --fea_dim 7 \
+  --patch_size 16 \
+  --n_hidden_dim 1000 \
+  --train_percent 0.5 \
+  --epochs 20 \
+  --batch_size 32 \
+  --patience 5 \
+  --add_nodes 10 \
+  --test_percent 0.4
+
+  python -u LSTF_forecasting.py \
+  --data ETTm2.csv \
+  --model_name ETTm2 \
+  --pre_train 512 \
+  --seq_len 96 \
+  --pred_len 336 \
+  --fea_dim 7 \
+  --patch_size 16 \
+  --n_hidden_dim 1000 \
+  --train_percent 0.5 \
+  --epochs 20 \
+  --batch_size 32 \
+  --patience 5 \
+  --add_nodes 10 \
+  --test_percent 0.4
+
+  python -u LSTF_forecasting.py \
+  --data ETTm2.csv \
+  --model_name ETTm2 \
+  --pre_train 512 \
+  --seq_len 96 \
+  --pred_len 720 \
+  --fea_dim 7 \
+  --patch_size 16 \
+  --n_hidden_dim 1000 \
+  --train_percent 0.5 \
+  --epochs 20 \
+  --batch_size 32 \
+  --patience 5 \
+  --add_nodes 10 \
+  --test_percent 0.4
+
